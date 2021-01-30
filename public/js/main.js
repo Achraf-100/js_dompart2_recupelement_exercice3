b897861ad2c650a17e19b9b328db6dd1c07e8dbf
Achraf <hardy1348@live.fr>
2021-01-30 02:25:39 +0100

    fin

---
+++ public/js/main.js
@@ -0,0 +1,32 @@
+// let test = document.querySelector('#content')
+// console.log(test);
+
+
+// let test1 = document.querySelectorAll('#content')
+// console.log(test);
+
+
+
+// let test3 = document.querySelector('.important')
+// console.log(test3);
+
+
+
+// let test4 = document.querySelectorAll('.important')
+// let transition = Array.from(test4)
+
+// transition.forEach(el => {
+//     let hoho = el.innerText
+//     console.log(hoho.substr(0, hoho.length-1) + hoho.substr(-1).toUpperCase());
+// });
+
+
+// let test7 = document.getElementById('content')
+// let test8 = test7.getElementsByClassName('grandParagraphe')
+// console.log(test8);
+
+
+
+
+
+
